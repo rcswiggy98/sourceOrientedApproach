9f6ea61a4595d5f29a9c1e8e06c5cf8b50e9df79
kfcaby <kfcaby@gmail.com>
2017-11-04 12:09:51 -0400

    added documentation to this function.

--- R/getMatchedDataset.R
+++ R/getMatchedDataset.R
@@ -1,3 +1,21 @@
+#' A one sentence description of what your function does
+#'
+#' A more detailed description of what the function is and how
+#' it works. It may be a paragraph that should not be separated
+#' by any spaces.
+#'
+#' @param inputParameter1 A description of the input parameter \code{inputParameter1}
+#' @param inputParameter2 A description of the input parameter \code{inputParameter2}
+#'
+#' @return output A description of the object the function outputs
+#'
+#' @keywords keywords
+#'
+#' @export
+#'
+#' @examples
+#' R code here showing how your function works
+
 getMatchedDataset <- function(dataset, regions, covariates, exact.vars = NA,
                               exposure.cutoff = 4, caliper = "default"){
 
